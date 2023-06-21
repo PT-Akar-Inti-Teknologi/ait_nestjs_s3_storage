dccb8e9f7e74f08f31c4cb65703a360159f68159
Taufik Rahadi <taufik.rahadi@akarinti.tech>
2023-06-21 08:47:48 +0700

    feature: delete file and getSignFile url

--- lib/services/s3.service.ts
+++ lib/services/s3.service.ts
@@ -4,18 +4,24 @@ import {
   S3,
   CompleteMultipartUploadCommandOutput,
   GetObjectCommand,
+  DeleteObjectCommand,
 } from '@aws-sdk/client-s3';
 import { Upload } from '@aws-sdk/lib-storage';
 import {
   UploadFileParams,
   UploadFileReturns,
 } from '../interfaces/upload-file.interface';
-import { Readable } from 'stream';
+import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
 
 @Injectable()
 export class S3Service {
   constructor(@Inject(getS3ConnectionToken()) private readonly s3: S3) {}
 
+  /**
+   * @description Upload your file to your s3 bucket
+   * @param options
+   * @returns
+   */
   public async uploadFile(
     options: UploadFileParams
   ): Promise<UploadFileReturns> {
@@ -38,6 +44,12 @@ export class S3Service {
     }
   }
 
+  /**
+   * 
+   * @param key 
+   * @param bucket 
+   * @returns 
+   */
   public async getFile(key: string, bucket: string) {
     try {
       const result = await this.s3.getObject({ Key: key, Bucket: bucket });
@@ -46,6 +58,41 @@ export class S3Service {
       return result;
     } catch (error) {
       throw error;
+    }
+  }
+
+  /**
+   * 
+   * @param key 
+   * @param bucket 
+   * @param expiresIn 
+   * @returns 
+   */
+  public async getSignedFileUrl(key: string, bucket: string, expiresIn: number) {
+    try {
+      const command = new GetObjectCommand({
+        Key: key,
+        Bucket: bucket
+      })
+
+      return await getSignedUrl(this.s3, command, {expiresIn})
+    } catch (error) {
+      throw error
+    }
+  }
+
+  public async deleteFile(key: string, bucket: string) {
+    try {
+      const command = new DeleteObjectCommand({
+        Bucket: bucket,
+        Key: key
+      })
+
+      const result = await this.s3.send(command)
+
+      return Boolean(result.DeleteMarker)
+    } catch (error) {
+      throw error
     }
   }
 }
